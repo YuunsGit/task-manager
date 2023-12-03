8e2f24001623143245aeb70b447ac7c1c9a303cd
Yunus Emre Kepenek <yunus.kepenek@radity.com>
2023-12-03 15:13:05 +0300

    Change database to neondb

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from "express";
 import bodyParser from "body-parser";
 import tasksRoutes from "./routes/tasks.routes.js";
 import { handle404, handleDbException, handleError } from "./middleware/error.mw.js";
+import cors from "cors";
 import "dotenv/config";
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/api/tasks", tasksRoutes);
 
